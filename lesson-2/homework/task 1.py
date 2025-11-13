82795cfbe1e52ff6e576a454d7d2223d8c1ecf3a
ShahbozShavkataliyev <shahbozshavkatov03@gmail.com>
2025-11-14 00:09:35 +0500

    Add basic user input and age calculation script

---
+++ lesson-2/homework/task 1.py
@@ -0,0 +1,13 @@
+name = input("enter your name: ")
+
+name
+
+birth_year = int(input("enter your year of birth: "))
+
+birth_year
+
+current_year = 2025
+
+age = current_year - birth_year
+
+print(f"Hello my name is {name}! I am {age} years old.") 
