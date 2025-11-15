c8f3eee4ddaf956e99657d6226644d9464762a92
ShahbozShavkataliyev <shahbozshavkatov03@gmail.com>
2025-11-15 22:06:14 +0500

    Add task 4 script for movie list and tuple creation

---
+++ lesson-3/homework/task 4.py
@@ -0,0 +1,7 @@
+movies = ['fast&furious', 'home alone', 'the godfather', 'goodfellas', 'the matrix']
+
+movies_tuple = tuple(['fast&furious', 'home alone', 'the godfather', 'goodfellas', 'the matrix'])
+
+movies_tuple
+
+type(movies_tuple)
