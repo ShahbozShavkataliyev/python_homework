3cff5b2243c03f04d72a94dd1a426822cb8e2dc4
ShahbozShavkataliyev <shahbozshavkatov03@gmail.com>
2025-11-17 12:58:43 +0500

    Create hw4.py

---
+++ lesson4/homework/hw4.py
@@ -0,0 +1,34 @@
+TASK1
+
+data = {"a": 5, "b": 2, "c": 9, "d": 1}
+
+sorted_list = sorted(data.values())
+
+sorted_list
+
+
+data = {"a": 5, "b": 2, "c": 9, "d": 1}
+
+sorted_data = sorted(data.values(),reverse = True)
+
+sorted_data
+
+TASK2
+
+num_dict = {0: 10, 1: 20}
+
+num_dict.update([(2,30)])
+
+num_dict
+
+TASK3
+
+dic1 = {1: 10, 2: 20}
+dic2 = {3: 30, 4: 40}
+dic3 = {5: 50, 6: 60}
+
+dic1.update(dic2)
+
+dic1.update(dic3)
+
+dic1
